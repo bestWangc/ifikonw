377b53d3950a18a0362eca88894d9aea53bc7320
wangc <wangchenphp@sina.com>
2020-03-05 17:37:03 +0800

    初步方法

--- application/index/controller/Base.php
+++ application/index/controller/Base.php
@@ -2,11 +2,52 @@
 namespace app\index\controller;
 
 use think\Controller;
+use think\Db;
 
 class Base extends Controller
 {
+    protected $memberID;
+
     public function initialize()
     {
-        echo '';
+        $this->getIP();
+    }
+
+    function getIP() {
+        $unknown = 'unknown';
+        $ip = '';
+        if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])
+            && $_SERVER['HTTP_X_FORWARDED_FOR']
+            && strcasecmp($_SERVER['HTTP_X_FORWARDED_FOR'], $unknown)) {
+            $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        } elseif (isset($_SERVER['REMOTE_ADDR']) && $_SERVER['REMOTE_ADDR'] && strcasecmp($_SERVER['REMOTE_ADDR'], $unknown)) {
+            $ip = $_SERVER['REMOTE_ADDR'];
+        }
+        //处理多层代理的情况
+        if (false !== strpos($ip, ',')) $ip = reset(explode(',', $ip));
+
+        if(!empty($ip)){
+            //判断数据库有没有此ip
+            $info = Db::name('member')->where('ip',$ip)->field('id,last_day,count')->find();
+            if(empty($info)){
+                $data = [
+                    'ip' => $ip,
+                    'last_day' => time()
+                ];
+                $insertID = Db::name('member')->insertGetId($data);
+            } else {
+                if(time() - $info['last_day'] > 86400){
+                    $data = [
+                        'ip' => $ip,
+                        'count' => $info['count'] + 1,
+                        'last_day' => time()
+                    ];
+                    Db::name('member')->where('id',$info['id'])->update($data);
+                }
+                $insertID = $info['id'];
+            }
+            $this->memberID = $insertID;
+        }
+        return $ip;
     }
 }
--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -2,11 +2,98 @@
 namespace app\index\controller;
 
 
+use think\Db;
+use think\facade\Cache;
+use think\facade\Request;
+
 class Index extends Base
 {
     public function index()
     {
+        $this->articleInfo();
+        // return $this->fetch();
+    }
 
-        return $this->fetch();
+    public function articleInfo()
+    {
+        $totalName = 'total-count';
+        $total = Cache::get($totalName);
+        if(!$total){
+            $count = Db::name('article')->count('id');
+            Cache::set($totalName,(int)$count);
+        }
+
+        //验证文章id
+        $hadRead = $this->getArticleID($this->memberID,$total);
+
+        return false;
+    }
+
+    private function getArticleID($memberID,$total)
+    {
+
+        $articleID = 0;
+        $readInfo = Db::name('read')->where('member_id',$memberID)->column('article_id');
+        dump($readInfo);
+        $aricleInfo = Db::name('article')->column('id');
+        dump($aricleInfo);
+        $choseArr = array_diff($aricleInfo,$readInfo);
+
+        if(!empty($choseArr)){
+            $rand = rand(0,count($choseArr)-1);
+            while(!isset($choseArr[$rand])){
+                $rand = rand(0,count($choseArr)-1);
+            }
+            $articleID = $choseArr[$rand];
+
+            Db::name('read')->insert(['member_id' => $memberID,'article_id' => $articleID]);
+        }
+
+        var_dump($articleID);
+        die;
+
+        $articleID = 0;
+
+        $randArr = [];
+        $articleID = rand(1,$total);
+        do{
+            do{
+                $articleID = rand(1,$total);
+                if(count($randArr) >= $total){
+                    var_dump($randArr);
+                    $articleID = 0;
+                    break;
+                }
+            }while(!in_array($articleID,$randArr) && array_push($randArr,$articleID));
+
+        }while($this->getReadInfo($memberID,$articleID));
+
+        var_dump($articleID);
+        return $articleID;
+    }
+
+    //看看该ip已经看过那些条目
+    private function getReadInfo($memberID,$articleID)
+    {
+        $res = false;
+        $name = $memberID.'-read-'.$articleID;
+        if(Cache::get($name)) {
+            $res = true;
+        }else{
+            var_dump('11111111111111');
+            $readInfo = Db::name('read')->where('member_id')->column('article_id');
+            var_dump($readInfo);
+            if(!empty($readInfo)){
+                foreach ($readInfo as $key){
+                    $cacheName = $memberID.'-read-'.$key;
+                    Cache::set($cacheName,1);
+                }
+            }
+            if(in_array($articleID,$readInfo)){
+                $res = true;
+            }
+        }
+
+        return $res;
     }
 }
